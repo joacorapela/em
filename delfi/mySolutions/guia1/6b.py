1bd28056b080911750046e85a9269e3c6ce7ea1d
joacorapela <joacorapela@gmail.com>
2026-09-09 22:07:55 +0100

    Fixed, the previous version was wrong :(

--- delfi/mySolutions/guia1/6b.py
+++ delfi/mySolutions/guia1/6b.py
@@ -2,34 +2,27 @@ import numpy as np
 import plotly.figure_factory as ff
 
 
-def E(p, q, d, epsilon0s=8.85*1e-6):
+def E_y(y, q, d, epsilon0s=8.85*1e-6):
     # q should be given in micro Coulomb
     # answer return in Newton / Coulomb
 
-    p_plus = -d/2 + 0j
-    p_minus = d/2 + 0j
-    E_plus = (1 / (4 * np.pi * epsilon0s) *
-              q * (p - p_plus) / np.abs(p - p_plus)**3)
-    E_minus = (1 / (4 * np.pi * epsilon0s) *
-               q * (p - p_minus) / np.abs(p - p_minus)**3)
-    E = E_plus + E_minus
-
-    return E
+    E_y = q * d / ((4 * np.pi * epsilon0s) * ((d / 2)**2 + y**2)**1.5)
+    return E_y
 
 
 # define constants
 q = 0.1  # micro Coulomb
 d = 8.0  # meter
-y_min = -10.0
-y_max = 10.0
+y_min = -30.0
+y_max = 30.0
 dy = 0.5
-xlim = (-d, d)
+xlim = (-1.0, 1.0)
 fig_filename_pattern = "../../figures/6b.{:s}"
 
 # Quiver parameters
-quiver_scale = 0.0007
+quiver_scale = 0.007
 quiver_arrow_scale = 0.3
-quiver_arrow_angle = np.pi / 4
+quiver_arrow_angle = np.pi / 3
 quiver_width = 3
 
 ys = np.arange(y_min, y_max, dy)
@@ -49,8 +42,8 @@ fig = ff.create_quiver(
 )
 fig.add_vline(x=-d/2)
 fig.add_vline(x=d/2)
-fig.update_yaxes(title="y")
-fig.update_xaxes(title="x", scaleratio=1, range=xlim)
+fig.update_yaxes(title="y", scaleanchor="x", scaleratio=1)
+fig.update_xaxes(title="x", range=xlim)
 fig.update_traces(line=dict(width=quiver_width))
 fig.update_layout(showlegend=False)
 fig.write_html(fig_filename_pattern.format("html"))
