095c8d844b5e1b016b41d7a95b67492b37edc9ab
joacorapela <joacorapela@gmail.com>
2026-09-13 19:10:08 +0100

    Added

---
+++ delfi/mySolutions/guia1/code/scripts/9c.py
@@ -0,0 +1,19 @@
+import math
+
+
+def E(z0, sigmas, epsilon0s, R):
+    answer = (0,
+              0,
+              (z0 * sigmas / (2 * epsilon0s) *
+               (1.0 / abs(z0) - 1.0 / math.sqrt(R**2 + z0**2))))
+    return answer
+
+
+# problem variables
+epsilon0s = 8.85  # scale factor 1e12
+z0 = 0.1
+R = 0.05
+sigmas = 0.5  # scale factor 1e6
+
+answer = E(z0=z0, sigmas=sigmas, epsilon0s=epsilon0s, R=R)
+print(f"answer=({answer[0]:.4f},{answer[1]:.4f},{answer[2]:.4f})")
